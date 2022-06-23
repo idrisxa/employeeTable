6f13bba37d72211bed9ecaaa0680d1680ae06948
idrisadebisi <85631468+idrisadebisi@users.noreply.github.com>
2022-06-23 15:55:17 -0400

    included form for adding new employee and installed json server

--- EmployeeTableCRUD/src/app/app.component.ts
+++ EmployeeTableCRUD/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+// import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EmployeeTableCRUD';
+
+  // closeResult: string = '';
+    
+  // /*------------------------------------------
+  // --------------------------------------------
+  // Created constructor
+  // --------------------------------------------
+  // --------------------------------------------*/
+  // constructor(private modalService: NgbModal) {}
+    
+  // /**
+  //  * Write code on Method
+  //  *
+  //  * @return response()
+  //  */
+  // open(content:any) {
+  //   this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+  //     this.closeResult = `Closed with: ${result}`;
+  //   }, (reason) => {
+  //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  //   });
+  // } 
+    
+  // /**
+  //  * Write code on Method
+  //  *
+  //  * @return response()
+  //  */
+  // private getDismissReason(reason: any): string {
+  //   if (reason === ModalDismissReasons.ESC) {
+  //     return 'by pressing ESC';
+  //   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+  //     return 'by clicking on a backdrop';
+  //   } else {
+  //     return  `with: ${reason}`;
+  //   }
+  // }
+
 }
--- EmployeeTableCRUD/src/app/employee-dashboard/employee-dashboard.component.ts
+++ EmployeeTableCRUD/src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms'
+//import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDashboardComponent implements OnInit {
 
-  constructor() { }
+  formValue !: FormGroup;
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      firstName : [''],
+      lastName : [''],
+      salary : ['']
+    })
   }
 
 }
+
