1893bf4f3f180a5bfe3b8e18d2e796f9dbe9e8ae
idrisadebisi <85631468+idrisadebisi@users.noreply.github.com>
2022-06-23 20:06:56 -0400

    created api put, get, put and delete routes

--- EmployeeTableCRUD/src/app/employee-dashboard/employee-dashboard.component.ts
+++ EmployeeTableCRUD/src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms'
+import { ApiService } from '../shared/api.service';
+import { EmployeeModel } from './employee-dashboard.model';
 //import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+
 
 
 @Component({
@@ -11,13 +15,53 @@ import {FormBuilder, FormGroup} from '@angular/forms'
 export class EmployeeDashboardComponent implements OnInit {
 
   formValue !: FormGroup;
-  constructor(private formbuilder: FormBuilder) { }
+  employeeModelObj : EmployeeModel = new EmployeeModel();
+  employeeData !: any;
+  constructor(private formbuilder: FormBuilder,
+  private api : ApiService) { }
 
   ngOnInit(): void {
     this.formValue = this.formbuilder.group({
+      id : [''],
       firstName : [''],
       lastName : [''],
       salary : ['']
+    })
+    this.getAllEmployee();
+  }
+
+  postEmployeeDetails(){
+    this.employeeModelObj.id = this.formValue.value.id;
+    this.employeeModelObj.firstName = this.formValue.value.firstName;
+    this.employeeModelObj.lastName = this.formValue.value.lastName;
+    this.employeeModelObj.salary = this.formValue.value.salary;
+
+    this.api.postEmployee(this.employeeModelObj)
+    .subscribe(res=>{
+      console.log(res);
+      alert("Employee Added Successfuly")
+      // let ref = document.getElementById('cancel')
+      // ref?.click();
+      this.formValue.reset();
+      this.getAllEmployee();
+    },
+    err=>{
+      alert("Something went wrong")
+    })
+  }
+  getAllEmployee(){
+    this.api.getEmployee()
+    .subscribe(res=>{
+      this.employeeData = res;
+    })
+  }
+
+  deleteEmployee(row : any){
+    this.api.deleteEmployee(row.id)
+    .subscribe(res=>{
+      alert("Employee Deleted");
+      this.getAllEmployee();
+      //this.employeeData = res;
     })
   }
 
